170fb6cad9ea0384f6cc004deda9c7102f6d83c1
Richard Mladek <rigospigos@gmail.com>
2026-03-01 15:59:35 +0100

    Update Turtle In color constants to green palette

--- src/assets/Colors.js
+++ src/assets/Colors.js
@@ -1,6 +1,6 @@
-export const LinkedInBlue = "#0073b1";
+export const LinkedInBlue = "#2e7d32";
 
-export const LinkedInLightBlue = "#70b5f9";
+export const LinkedInLightBlue = "#66bb6a";
 
 export const LinkedInBgColor = "#f3f2ef";
 
