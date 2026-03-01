7e18e628746773dc1c000c00c6f752d4ed31f93d
Richard Mladek <rigospigos@gmail.com>
2026-03-01 21:18:50 +0100

    Remove fake sidebar bottom UI

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import SidebarTop from "./sidebarTop/SidebarTop";
-import SidebarBottom from "./sidebarBottom/SidebarBottom";
 
 const Style = makeStyles((theme) => ({
   sidebar: {
@@ -16,7 +15,6 @@ const Sidebar = () => {
   return (
     <div className={classes.sidebar}>
       <SidebarTop />
-      <SidebarBottom />
     </div>
   );
 };
